006fc9a8ebdc49afdd520487ab55ee09f0880ea9
adilsoukaini <47067476+adilsoukaini@users.noreply.github.com>
2023-02-18 16:45:49 +0100

    done

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -4,44 +4,65 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  Keyboard,
 } from "react-native";
 import React from "react";
 import Task from "./Task";
-import { SparklesIcon } from "react-native-heroicons/solid";
-import { SparklesIcon as SparklesIconOutline } from "react-native-heroicons/outline";
-// Old solid style from heroicons v1
-import { SparklesIcon as SparklesIconMini } from "react-native-heroicons/mini";
 
 const HomeScreen = () => {
-  const [text, onChangeText] = React.useState("write a task");
+  const [task, setTask] = React.useState();
+  const [TasksItems, setTasksItems] = React.useState([]);
 
+  const handleAddTask = () => {
+    console.log(TasksItems);
+    Keyboard.dismiss();
+    setTasksItems([...TasksItems, task]);
+    setTask(null);
+  };
+  const deleteTask = (index) => {
+    console.log(TasksItems);
+    let copyitems = [...TasksItems];
+    copyitems.splice(index, 1);
+    setTasksItems(copyitems);
+  };
   return (
     <View className="p-5 relative h-full w-full">
       <View>
-        <Text className="text-xl font-bold ">Today's Task</Text>
+        <Text className="text-xl font-hfhbold ">Today's Task</Text>
       </View>
       <View className="mt-6">
         <ScrollView>
-          {this.state.names.map((item, index) => (
-            <View key={item.id} >
-              <Task Text={text} />
-            </View>
-          ))}
-          
+          {Array.isArray(TasksItems)
+            ? TasksItems.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={(index) => deleteTask(index)}
+                  >
+                    <Task text={item}></Task>
+                  </TouchableOpacity>
+                );
+              })
+            : console.log("not a tast")}
         </ScrollView>
       </View>
       <View className=" absolute bottom-[37] ">
         <View className="bg-white  h-[45] w-[260] rounded-3xl  ml-5 shadow-lg shadow-indigo-500/40">
           <TextInput
+            autoFocus={true}
+            placeholder="Enter Task here"
             className="text-center text-[#C0C0C0] pt-2"
-            onChangeText={onChangeText}
-            value={text}
+            onChangeText={(Text) => setTask(Text)}
+            value={task}
           />
         </View>
       </View>
 
       <View className="absolute right-[29] bottom-7">
-        <TouchableOpacity className="bg-white h-16 w-16 relative rounded-full ">
+        <TouchableOpacity
+          onPress={() => handleAddTask()}
+          className="bg-white h-16 w-16 relative rounded-full "
+        >
           <Text className="text-[50px] h-full w-full  absolute left-4 bottom-[4] text-[#C0C0C0] ">
             +
           </Text>
--- components/Task.js
+++ components/Task.js
@@ -6,7 +6,7 @@ const Task = (props) => {
     <View className="bg-[#FFFFFF] drop-shadow rounded-lg p-[15] justify-between flex-row mb-4  align-middle shadow-lg shadow-indigo-500/40">
       <View className="flex-row flex-wrap space-x-4 ">
         <TouchableOpacity className="bg-[#55BCF6] h-6 w-6 opacity-40 rounded-sm"></TouchableOpacity>
-        <Text>{props.Text}</Text>
+        <Text>{props.text}</Text>
       </View>
       <View className="mt-1 align-middle">
         <TouchableOpacity className="bg-white h-3 w-3 opacity-40 rounded-full border-4 border-[#55BCF6]  "></TouchableOpacity>
